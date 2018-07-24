167ff16a614555b81efd12a57402b993d2bf736b
mratsim <mamy_github@numforge.co>
2018-07-24 17:53:01 +0200

    Introduce types and add some boolean primitives

---
+++ hardy/datatypes.nim
@@ -0,0 +1,23 @@
+# Hardy
+# Copyright (c) 2018 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at http://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at http://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+type
+  HardBase* = SomeUnsignedInt or byte
+
+  HardBool*[T: HardBase] = range[T(0)..T(1)]
+    ## To avoid the compiler replacing bitwise boolean operations
+    ## by conditional branches, we don't use booleans.
+    ## We use an int to prevent compiler "optimization" and introduction of branches
+
+  Hard*[T: HardBase] = distinct openarray[T]
+    ## Hardy primitives are memory-backend agnostic.
+    ## Hardy integers can be stored in an opaque stack array
+    ## or a seq or even a string.
+    ##
+    ## Allocations is left to the client library.
+    ## Note that constant-time allocation is very involved for
+    ## heap-allocated types (i.e. requires a memory pool)
