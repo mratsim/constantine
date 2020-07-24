e80fff69a9f04012135a7e212a97b4fa5d67c382
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2020-07-24 20:02:14 +0200

    Update README closes #66

--- constantine/primitives/macro_assembler_x86.nim
+++ constantine/primitives/macro_assembler_x86.nim
@@ -6,7 +6,7 @@
 #   * Apache v2 license (license terms in the root directory or at http://www.apache.org/licenses/LICENSE-2.0).
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-import macros, strutils, sets, hashes
+import std/[macros, strutils, sets, hashes]
 
 # A compile-time inline assembler
 
