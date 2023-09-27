8e9e520a9170d57c347b184a447bc600cedff0eb
Agnish Ghosh <iamagnix@gmail.com>
2023-09-27 21:16:00 +0530

    fix: transcript func naming

--- constantine/commitments/ipa/transcript_gen.nim
+++ constantine/commitments/ipa/transcript_gen.nim
@@ -29,53 +29,53 @@ type
   EC_P* = ECP_TwEdwards_Prj[Fp[Banderwagon]]
   EC_P_Fr* = ECP_TwEdwards_Prj[Fr[Banderwagon]]
 
-func new_transcript_gen*[Transcript](res: var Transcript, label: seq[byte]) =
+func newTranscriptGen*[Transcript](res: var Transcript, label: openArray[byte]) =
     res.init()
     res.update(label)
 
 
-func message_append* [Transcript]( res: var Transcript, message: seq[byte], label: seq[byte]) =
+func messageAppend* [Transcript]( res: var Transcript, message: openArray[byte], label: openArray[byte]) =
     res.init()
     res.update(label)
     res.update(message)
 
 
-func message_append_u64* [Transcript](res: var Transcript, label: seq[byte], num_value: uint64) = 
+func messageAppend_u64* [Transcript](res: var Transcript, label: openArray[byte], num_value: uint64) = 
     res.init()
     res.update(label)
     res.update(num_value.toBytes(bigEndian))
 
-func domain_separator* [Transcript](res: var Transcript, label: seq[byte]) =
+func domainSeparator* [Transcript](res: var Transcript, label: openArray[byte]) =
     var state {.noInit.} : sha256
     state.update(label)
 
 
-func point_append* [Transcript] (res: var Transcript, label: seq[byte], point: EC_P) =
+func pointAppend* [Transcript] (res: var Transcript, label: openArray[byte], point: EC_P) =
     var bytes {.noInit.}: array[32, byte]
     doAssert point.serialize(bytes) == cttCodecEcc_Success
-    res.message_append(bytes, label)
+    res.messageAppend(bytes, label)
 
 
-func scalar_append* [Transcript] (res: var Transcript, label: seq[byte], scalar: EC_P_Fr) =
+func scalarAppend* [Transcript] (res: var Transcript, label: openArray[byte], scalar: EC_P_Fr) =
     var bytes {.noInit.}: array[32, byte]
 
     doAssert scalar.serialize(bytes) == cttCodecEcc_Success
-    res.message_append(bytes, label)
+    res.messageAppend(bytes, label)
 
 
 
 ## Generating Challenge Scalars based on the Fiat Shamir method
-func generate_challenge_scalar_multiproof* [EC_P_Fr] (gen: var EC_P_Fr, label: seq[byte]) =
+func generateChallengeScalar* [EC_P_Fr] (gen: var EC_P_Fr, label: openArray[byte]) =
     var state {.noInit.}: Transcript
     state.init()
-    state.domain_separator(label)
+    state.domainSeparator(label)
 
     var hash: array[32, byte]
     state.finish(hash)
 
     doAssert hash.deserialize(gen) == cttCodecEcc_Success
 
-    state.scalar_append(label, gen)
+    state.scalarAppend(label, gen)
 
 
     
