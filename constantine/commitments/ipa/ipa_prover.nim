82cc46e5ef34ce4d30b0b7d22851c53c14966792
Agnish Ghosh <iamagnix@gmail.com>
2023-10-01 23:33:36 +0530

    add: init createIPAProof()

--- constantine/commitments/ipa/ipa_prover.nim
+++ constantine/commitments/ipa/ipa_prover.nim
@@ -35,8 +35,70 @@ type
      R_vector: openArray[EC_P]
      A_scalar: EC_P_Fr
 
+const
+ DOMAIN: uint64 = 256
+
 func createIPAProof*[IPAProof] (res: var IPAProof, transcript: Transcript, ic: IPASettings, commitment: EC_P, a: openArray[EC_P_Fr], evalPoint: EC_P_Fr )=
   transcript.domain_separator(asBytes"ipa")
+  var b {.noInit.}: array[DOMAIN, EC_P_Fr]
+  
+  b = ic.PrecomputedWeights.computeBarycentricCoefficients(evalPoint)
+  var innerProd {.noInit.}: EC_P_Fr
+
+  var check {.noInit.}: bool
+  
+  check = innerProd.computeInnerProducts(a,b.toBig())
+
+  assert check == true, "Could not compute the Inner Product"
+
+  transcript.pointAppend(commitment, asBytes"C")
+  transcript.scalarAppend(evalPoint, asBytes"input point")
+  transcript.scalarAppend(innerProd, asBytes"output point")
+
+  let w = transcript.generateChallengeScalar("w")
+
+  var q {.noInit.} : EC_P
+  q.scalarMul(ic.Q_val, w.toBig())
+
+  let num_rounds = ic.numRounds
+
+  let current_basis = ic.SRS
+
+  var L {.noInit.}: array[num_rounds, EC_P]
+
+  var R {.noInit.}: array[num_rounds, EC_P]
+
+  for i in 0..<int(num_rounds):
+    
+    var a_L, a_R {.noInit.}: EC_P_Fr
+
+    a_L.toStridedView()
+    a_R.toStridedView()
+
+    (a_L, a_R).splitScalars()
+
+    var b_L, b_R {.noInit.}: EC_P_Fr
+
+    b_L.toStridedView()
+    b_R.toStridedView()
+
+    (b_L, b_R).splitScalars()
+
+    var G_L, G_R {.noInit.}: EC_P
+
+    (G_L, G_R).splitPoints()
+
+    var z_L {.noInit.}: EC_P_Fr
+    z_L.computeInnerProducts( )
+
+
+
+
+
+
+
 
   
+
+
 
