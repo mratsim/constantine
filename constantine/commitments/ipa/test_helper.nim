703f4bfc03e0cf6bdfcad01ee2b3af2eb606369e
Agnish Ghosh <iamagnix@gmail.com>
2023-10-10 13:44:51 +0530

    add: helper types for code clarity

--- constantine/commitments/ipa/test_helper.nim
+++ constantine/commitments/ipa/test_helper.nim
@@ -7,7 +7,8 @@
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
 import 
-    ./multiproof,
+    ./[multiproof],
+    ./helper_types,
     ../../../constantine/math/config/[type_ff, curves],
     ../../../constantine/math/elliptic/[
      ec_twistededwards_affine,
@@ -24,76 +25,59 @@ import
 
 # Please refer to https://hackmd.io/mJeCRcawTRqr9BooVpHv5g 
 
-type EFr* = Fr[Banderwagon]
-
-type
-  EC_P* = ECP_TwEdwards_Prj[Fp[Banderwagon]]
-  EC_P_Fr* = EFr
-  Bytes* = array[32, byte]
-
-type 
-    Point* = object 
-     x: EFr
-     y: EFr
-
-type 
-    Points* =  seq[Point]
-
-type Poly* = seq[EFr]
-
-func evaluate* [EC_P_Fr] (point: var EC_P_Fr, poly: Poly) = 
+func evaluate* [EC_P_Fr] (point: var EC_P_Fr, poly: openArray[EC_P_Fr], n: static int) = 
     var powers {.noInit.}: array[2,EC_P_Fr]
     powers.computePowersOfElem(point, poly.len)
 
-    var total {.noInit.} : EFr
+    var total {.noInit.} : EC_P_Fr
     total.setZero()
 
     for i in 0..<poly.len:
-        var tmp {.noInit.} : EFr
+        var tmp {.noInit.} : EC_P_Fr
         tmp.prod(powers[i], poly[i])
         total += tmp
 
     point = total
 
+func truncate* [EC_P_Fr] (res: var openArray[EC_P_Fr], s: openArray[EC_P_Fr], to: int, n: static int)=
+    for i in 0..<to:
+        res[i] = s[i]
 
-
-func interpolate*[Poly](res: var Poly, points: Points) =
+func interpolate* [EC_P_Fr] (res: var openArray[EC_P_Fr], points: openArray[Coord], n: static int) =
     
-    var one {.noInit.} : EFr
+    var one {.noInit.} : EC_P_Fr
     one.setOne()
 
-    var zero {.noInit.} : EFr
+    var zero {.noInit.} : EC_P_Fr
     zero.setZero()
 
     var max_degree_plus_one {.noInit.}: int
     max_degree_plus_one = points.len
 
-    doAssert max_degree_plus_one < 2, "Should be interpolating for degree >= 1!"
-
-    var coeffs {.noInit.} : Poly
+    doAssert (max_degree_plus_one < 2).bool() == false, "Should be interpolating for degree >= 1!"
 
     for k in 0..<points.len:
 
-        var point {.noInit.} : Point
+        var point: Coord
         point = points[k]
 
-        var x_k {.noInit.} : EFr 
+        var x_k : EC_P_Fr 
         x_k = point.x
-        var y_k {.noInit.} : EFr 
+        var y_k  : EC_P_Fr 
         y_k = point.y
 
-        var contribution {.noInit.}: seq[EFr]
-        var denominator {.noInit.}: EFr
+        var contribution : array[n,EC_P_Fr]
+        var denominator {.noInit.}: EC_P_Fr
         denominator.setOne()
 
 
         var max_contribution_degree = 0
 
-        for j in 0..points.len:
+        for j in 0..<points.len:
 
-            var point {.noInit.}: Point 
+            var point {.noInit.}: Coord 
             point = points[j]
-            var x_j {.noInit.} : EFr 
+            var x_j {.noInit.} : EC_P_Fr 
             x_j = point.x
 
             if j == k:
@@ -112,15 +96,17 @@ func interpolate*[Poly](res: var Poly, points: Points) =
 
             else:
 
-                var mul_by_minus_x_j {.noInit}: seq[EFr]
-                for _, el in  contribution:
-                    var tmp : EFr = el
+                var mul_by_minus_x_j : array[n,EC_P_Fr]
+                for el in contribution:
+                    var tmp : EC_P_Fr = el
                     tmp *= x_j
                     tmp.diff(zero,tmp)
-                    mul_by_minus_x_j.add(tmp)
+                    var idx = 0
+                    mul_by_minus_x_j[idx] = tmp
+                    idx = idx + 1
 
                 contribution[0] = zero
-                contribution = contribution[0..<max_degree_plus_one]
+                contribution.truncate(contribution, max_degree_plus_one, n)
 
                 doAssert not(max_degree_plus_one == mul_by_minus_x_j.len), "Malformed mul_by_minus_x_j!"
 
@@ -137,17 +123,17 @@ func interpolate*[Poly](res: var Poly, points: Points) =
             var tmp = contribution[i]
             tmp *= contribution[i]
             tmp *= y_k
-            coeffs[i].sum(coeffs[i], tmp)
+            res[i].sum(res[i], tmp)
 
         
-    res=coeffs
 
 
-# func *setEval [EFr] (res: var EFr, x : EFr)=
 
-#     var tmp_a {.noInit.} : EFr
+# func *setEval [EC_P_Fr] (res: var EC_P_Fr, x : EC_P_Fr)=
 
-#     var one {.noInit.}: EFr
+#     var tmp_a {.noInit.} : EC_P_Fr
+
+#     var one {.noInit.}: EC_P_Fr
 #     one.setOne()
 
 #     tmp_a.diff(x, one)
