mratsim/constantine

commit 4ccd8aaab86f0c1955125a5ce6fe66915cb2cbf4

EVM modexp: solve DOS vectors (#286)
* stash prep for Barret Reduction

* benches lost in rebase

* fix vartime reduction

* some improvement and fixes on reduce_vartime

* Fuse reductions when converting to Montgomery + use window=1 in powMont for small exponents. ~2.7x to 3.3x accel

* modexp: Introduce a no-reduction path for small base+exponent compared to modulus. Fix DOS

* optim for padded exponents

* remove commented out code [skip ci]

* Missing noInline for allocStackArray

constantine/platforms/intrinsics/extended_precision_vartime.nim
@@ -1,0 +1,122 @@
+# Constantine
+# Copyright (c) 2018-2019    Status Research & Development GmbH
+# Copyright (c) 2020-Present Mamy André-Ratsimbazafy
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at http://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at http://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+# ############################################################
+#
+#              Variable-time extended-precision
+#                    compiler intrinsics
+#
+# ############################################################
+
+import ../abstractions
+
+func div2n1n_nim_vartime[T: SomeUnsignedInt](q, r: var T, n_hi, n_lo, d: T) {.tags:[VarTime].}=
+  ## Division uint128 by uint64
+  ## Warning ⚠️ :
+  ##   - if n_hi == d, quotient does not fit in an uint64 and will throw SIGFPE
+  ##   - if n_hi > d result is undefined
+
+  # doAssert leadingZeros(d) == 0, "Divisor was not normalized"
+
+  const
+    size = sizeof(q) * 8
+    halfSize = size div 2
+    halfMask = (1.T shl halfSize) - 1.T
+
+  func halfQR(n_hi, n_lo, d, d_hi, d_lo: T): tuple[q, r: T] {.nimcall.} =
+
+    var (q, r) = (n_hi div d_hi, n_hi mod d_hi)
+    let m = q * d_lo
+    r = (r shl halfSize) or n_lo
+
+    # Fix the reminder, we're at most 2 iterations off
+    if r < m:
+      dec q
+      r += d
+      if r >= d and r < m:
+        dec q
+        r += d
+    r -= m
+    (q, r)
+
+  let
+    d_hi = d shr halfSize
+    d_lo = d and halfMask
+    n_lohi = n_lo shr halfSize
+    n_lolo = n_lo and halfMask
+
+  # First half of the quotient
+  let (q1, r1) = halfQR(n_hi, n_lohi, d, d_hi, d_lo)
+
+  # Second half
+  let (q2, r2) = halfQR(r1, n_lolo, d, d_hi, d_lo)
+
+  q = (q1 shl halfSize) or q2
+  r = r2
+
+when sizeof(int) == 8 and defined(vcc):
+  func udiv128_vartime(highDividend, lowDividend, divisor: uint64, remainder: var uint64): uint64 {.importc:"_udiv128", header: "<intrin.h>", nodecl, tags:[VarTime].}
+    ## Division 128 by 64, Microsoft only, 64-bit only,
+    ## returns quotient as return value remainder as var parameter
+    ## Warning ⚠️ :
+    ##   - if n_hi == d, quotient does not fit in an uint64 and will throw SIGFPE
+    ##   - if n_hi > d result is undefined
+
+  func div2n1n_128_vartime(q, r: var uint64, n_hi, n_lo, d: uint64) {.inline.}=
+      ## Division uint128 by uint64
+      ## Warning ⚠️ :
+      ##   - if n_hi == d, quotient does not fit in an uint64 and will throw SIGFPE
+      ##   - if n_hi > d result is undefined
+      q = udiv128_vartime(n_hi, n_lo, d, r)
+
+elif sizeof(int) == 8 and GCC_Compatible:
+  type
+    uint128{.importc: "unsigned __int128".} = object
+
+  const
+    newerNim = (NimMajor, NimMinor) > (1, 6)
+    noExplicitPtrDeref = defined(cpp) or newerNim
+
+  func div2n1n_128_vartime(q, r: var uint64, n_hi, n_lo, d: uint64) {.inline, tags:[VarTime].}=
+    ## Division uint128 by uint64
+    ## Warning ⚠️ :
+    ##   - if n_hi == d, quotient does not fit in an uint64 and will throw SIGFPE on some platforms
+    ##   - if n_hi > d result is undefined
+    var dblPrec {.noinit.}: uint128
+    {.emit:[dblPrec, " = (unsigned __int128)", n_hi," << 64 | (unsigned __int128)",n_lo,";"].}
+
+    # Don't forget to dereference the var param in C mode
+    when noExplicitPtrDeref:
+      {.emit:[q, " = (NU64)(", dblPrec," / ", d, ");"].}
+      {.emit:[r, " = (NU64)(", dblPrec," % ", d, ");"].}
+    else:
+      {.emit:["*",q, " = (NU64)(", dblPrec," / ", d, ");"].}
+      {.emit:["*",r, " = (NU64)(", dblPrec," % ", d, ");"].}
+
+
+func div2n1n_vartime*(q, r: var SecretWord, n_hi, n_lo, d: SecretWord) {.inline.} =
+  ## Division uint128 by uint64
+  ## Warning ⚠️ :
+  ##   - if n_hi == d, quotient does not fit in an uint64 and will throw SIGFPE
+  ##   - if n_hi > d result is undefined
+  ##
+  ## To avoid issues, n_hi, n_lo, d should be normalized.
+  ## i.e. shifted (== multiplied by the same power of 2)
+  ## so that the most significant bit in d is set.
+  when sizeof(int) == 4:
+    let dividend = (uint64(n_hi) shl 32) or uint64(n_lo)
+    let divisor = uint64(d)
+    q = uint32(dividend div divisor)
+    r = uint32(dividend mod divisor)
+  when nimvm:
+    div2n1n_nim_vartime(BaseType q, BaseType r, BaseType n_hi, BaseType n_lo, BaseType d)
+  else:
+    when declared(div2n1n_128_vartime):
+      div2n1n_128_vartime(BaseType q, BaseType r, BaseType n_hi, BaseType n_lo, BaseType d)
+    else:
+      div2n1n_nim_vartime(BaseType q, BaseType r, BaseType n_hi, BaseType n_lo, BaseType d)

tests/math_arbitrary_precision/t_bigints_mod.nim
@@ -3,17 +3,20 @@
 import
   ../../constantine/math/[
     arithmetic,
-    io/io_bigints]
+    io/io_bigints],
+  ../../constantine/math_arbitrary_precision/arithmetic/limbs_divmod_vartime
 
 let a = BigInt[64].fromUint(0xa0e5cb56a1c08396'u64)
 let M = BigInt[64].fromUint(0xae57180eceb0206f'u64)
 
-var r: BigInt[64]
+var r, r2: BigInt[64]
 
 r.reduce(a, M)
+doAssert r2.limbs.reduce_vartime(a.limbs, M.limbs)
 
 let rU64 = 0xa0e5cb56a1c08396'u64 mod 0xae57180eceb0206f'u64
-echo r.toHex()
+# echo r.toHex()
 
 doAssert rU64 == a.limbs[0].uint64
 doAssert bool(a == r)
+echo "SUCCESS: t_bigints_mod.nim"

benchmarks/bench_gmp_modmul.nim
@@ -14,6 +14,7 @@ import
   # Internal
   ../constantine/math/io/io_bigints,
   ../constantine/math/arithmetic,
+  ../constantine/math_arbitrary_precision/arithmetic/limbs_divmod_vartime,
   ../constantine/platforms/abstractions,
   ../constantine/serialization/codecs,
   # Test utilities
@@ -124,6 +125,13 @@ proc main() =
     let stopCTTMod = getMonoTime()
     echo "Constantine - ", aBits, " mod ", bBits, "           -> ", bBits,  " mod: ", float(inNanoseconds((stopCTTmod-startCTTmod)))/float(NumIters), " ns"
 
+    let startCTTvartimeMod = getMonoTime()
+    var q {.noInit.}: BigInt[bBits]
+    for _ in 0 ..< NumIters:
+      discard divRem_vartime(q.limbs, rTestMod.limbs, aTest.limbs, bTest.limbs)
+    let stopCTTvartimeMod = getMonoTime()
+    echo "Constantine - ", aBits, " mod ", bBits, " (vartime) -> ", bBits,  " mod: ", float(inNanoseconds((stopCTTvartimeMod-startCTTvartimeMod)))/float(NumIters), " ns"
+
     echo "----"
     # Modular reduction - double-size
 
@@ -139,7 +147,12 @@ proc main() =
     let stopCTTMod2 = getMonoTime()
     echo "Constantine - ", rBits, " mod ", bBits, "           -> ", bBits,  " mod: ", float(inNanoseconds((stopCTTmod2-startCTTmod2)))/float(NumIters), " ns"
 
-    # Constantine
+    let startCTTvartimeMod2 = getMonoTime()
+    var q2 {.noInit.}: BigInt[bBits]
+    for _ in 0 ..< NumIters:
+      discard divRem_vartime(q2.limbs, rTestMod.limbs, rTest.limbs, bTest.limbs)
+    let stopCTTvartimeMod2 = getMonoTime()
+    echo "Constantine - ", rBits, " mod ", bBits, " (vartime) -> ", bBits,  " mod: ", float(inNanoseconds((stopCTTvartimeMod2-startCTTvartimeMod2)))/float(NumIters), " ns"
 
     echo ""
 

constantine/math/arithmetic/limbs_extmul.nim
@@ -56,9 +56,8 @@ func prod_comba[rLen, aLen, bLen: static int](r: var Limbs[rLen], a: Limbs[aLen]
       u = t
       t = Zero
 
-  if aLen+bLen < rLen:
-    for i in aLen+bLen ..< rLen:
-      r[i] = Zero
+  for i in aLen+bLen ..< rLen:
+    r[i] = Zero
 
 func prod*[rLen, aLen, bLen: static int](r{.noalias.}: var Limbs[rLen], a: Limbs[aLen], b: Limbs[bLen]) {.inline.} =
   ## Multi-precision multiplication
@@ -156,9 +155,8 @@ func square_Comba[rLen, aLen](
       u = t
       t = Zero
 
-  if aLen*2 < rLen:
-    for i in aLen*2 ..< rLen:
-      r[i] = Zero
+  for i in aLen*2 ..< rLen:
+    r[i] = Zero
 
 func square_operandScan[rLen, aLen](
        r: var Limbs[rLen],

constantine/math_arbitrary_precision/arithmetic/bigints_views.nim
@@ -15,7 +15,7 @@ import
   ./limbs_mod2k,
   ./limbs_multiprec,
   ./limbs_extmul,
-  ./limbs_divmod
+  ./limbs_divmod_vartime
 
 # No exceptions allowed
 {.push raises: [], checks: off.}
@@ -41,7 +41,34 @@ import
 # Also need to take into account constant-time for RSA
 # i.e. countLeadingZeros can only be done on public moduli.
 
-func powOddMod_vartime*(
+iterator unpackBE(scalarByte: byte): bool =
+  for i in countdown(7, 0):
+    yield bool((scalarByte shr i) and 1)
+
+func pow_vartime(
+       r: var openArray[SecretWord],
+       a: openArray[SecretWord],
+       exponent: openArray[byte]) {.tags:[VarTime, Alloca], meter.} =
+  ## r <- a^exponent
+
+  r.setOne()
+  var isOne = true
+
+  for e in exponent:
+    for bit in unpackBE(e):
+      if not isOne:
+        r.square_vartime(r)
+      if bit:
+        if isOne:
+          for i in 0 ..< a.len:
+            r[i] = a[i]
+          for i in a.len ..< r.len:
+            r[i] = Zero
+          isOne = false
+        else:
+          r.prod_vartime(r, a)
+
+func powOddMod_vartime(
        r: var openArray[SecretWord],
        a: openArray[SecretWord],
        exponent: openArray[byte],
@@ -55,33 +82,20 @@ func powOddMod_vartime*(
   debug:
     doAssert bool(M.isOdd())
 
-  let aBits  = a.getBits_LE_vartime()
   let mBits  = M.getBits_LE_vartime()
   let eBits  = exponent.getBits_BE_vartime()
 
   if eBits == 1:
-    r.view().reduce(a.view(), aBits, M.view(), mBits)
+    discard r.reduce_vartime(a, M)
     return
 
   let L      = wordsRequired(mBits)
   let m0ninv = M[0].negInvModWord()
-  var rMont  = allocStackArray(SecretWord, L)
 
-  block:
-    var r2Buf = allocStackArray(SecretWord, L)
-    template r2: untyped = r2Buf.toOpenArray(0, L-1)
-    r2.r2_vartime(M.toOpenArray(0, L-1))
+  var aMont_buf = allocStackArray(SecretWord, L)
+  template aMont: untyped = aMont_buf.toOpenArray(0, L-1)
 
-    # Conversion to Montgomery can auto-reduced by up to M*R
-    # if we use redc2xMont (a/R) and montgomery multiplication by R³
-    # For now, we call explicit reduction as it can handle all sizes.
-    # TODO: explicit reduction uses constant-time division which is **very** expensive
-    if a.len != M.len:
-      let t = allocStackArray(SecretWord, L)
-      t.LimbsViewMut.reduce(a.view(), aBits, M.view(), mBits)
-      rMont.LimbsViewMut.getMont(LimbsViewConst t, M.view(), LimbsViewConst r2.view(), m0ninv, mBits)
-    else:
-      rMont.LimbsViewMut.getMont(a.view(), M.view(), LimbsViewConst r2.view(), m0ninv, mBits)
+  aMont.getMont_vartime(a, M)
 
   block:
     var oneMontBuf = allocStackArray(SecretWord, L)
@@ -91,12 +105,11 @@ func powOddMod_vartime*(
     let scratchLen = L * ((1 shl window) + 1)
     var scratchSpace = allocStackArray(SecretWord, scratchLen)
 
-    rMont.LimbsViewMut.powMont_vartime(
+    aMont_buf.LimbsViewMut.powMont_vartime(
       exponent, M.view(), LimbsViewConst oneMontBuf,
       m0ninv, LimbsViewMut scratchSpace, scratchLen, mBits)
 
-  r.view().fromMont(LimbsViewConst rMont, M.view(), m0ninv, mBits)
-
+  r.view().fromMont(LimbsViewConst aMont_buf, M.view(), m0ninv, mBits)
 
 func powMod_vartime*(
        r: var openArray[SecretWord],
@@ -126,6 +139,14 @@ func powMod_vartime*(
     r[0] = a[0]
     for i in 1 ..< r.len:
       r[i] = Zero
+    return
+
+  # No modular reduction needed
+  # -------------------------------------------------------------------
+  if eBits < WordBitWidth and
+     aBits.uint shr (WordBitWidth - eBits) == 0 and # handle overflow of uint128 [0, aBits] << eBits
+     aBits.uint shl eBits < mBits.uint:
+    r.pow_vartime(a, exponent)
     return
 
   # Odd modulus
@@ -197,5 +218,5 @@ func powMod_vartime*(
 
   var qyBuf = allocStackArray(SecretWord, M.len)
   template qy: untyped = qyBuf.toOpenArray(0, M.len-1)
-  qy.prod(q, y)
+  qy.prod_vartime(q, y)
   discard r.addMP(qy, a1)

constantine/math_arbitrary_precision/arithmetic/limbs_divmod.nim
@@ -12,7 +12,7 @@ import
   ./limbs_fixedprec
 
 # No exceptions allowed
-{.push raises: [].}
+{.push raises: [], checks: off.}
 
 # ############################################################
 #
@@ -63,7 +63,7 @@ func shlAddMod_estimate(a: LimbsViewMut, aLen: int,
     a1 = (a[^1] shl (WordBitWidth-R)) or (a[^2] shr R)
     m0 = (M[^1] shl (WordBitWidth-R)) or (M[^2] shr R)
 
-  # m0 has its high bit set. (a0, a1)/p0 fits in a limb.
+  # m0 has its high bit set. (a0, a1)/m0 fits in a limb.
   # Get a quotient q, at most we will be 2 iterations off
   # from the true quotient
   var q, r: SecretWord
@@ -78,29 +78,29 @@ func shlAddMod_estimate(a: LimbsViewMut, aLen: int,
 
   # Now substract a*2^64 - q*p
   var carry = Zero
-  var over_p = CtTrue                            # Track if quotient greater than the modulus
+  var overM = CtTrue                             # Track if quotient greater than the modulus
 
   for i in 0 ..< MLen:
-    var qp_lo: SecretWord
+    var qm_lo: SecretWord
 
-    block: # q*p
-      # q * p + carry (doubleword) carry from previous limb
-      muladd1(carry, qp_lo, q, M[i], carry)
+    block: # q*m
+      # q * m + carry (doubleword) carry from previous limb
+      muladd1(carry, qm_lo, q, M[i], carry)
 
     block: # a*2^64 - q*p
       var borrow: Borrow
-      subB(borrow, a[i], a[i], qp_lo, Borrow(0))
+      subB(borrow, a[i], a[i], qm_lo, Borrow(0))
       carry += SecretWord(borrow) # Adjust if borrow
 
-    over_p = mux(a[i] == M[i], over_p, a[i] > M[i])
+    overM = mux(a[i] == M[i], overM, a[i] > M[i])
 
   # Fix quotient, the true quotient is either q-1, q or q+1
   #
-  # if carry < q or carry == q and over_p we must do "a -= p"
-  # if carry > hi (negative result) we must do "a += p"
+  # if carry < q or carry == q and over_p we must do "a -= m"
+  # if carry > hi (negative result) we must do "a += m"
 
   result.neg = carry > hi
-  result.tooBig = not(result.neg) and (over_p or (carry < hi))
+  result.tooBig = not(result.neg) and (overM or (carry < hi))
 
 func shlAddMod(a: LimbsViewMut, aLen: int,
                c: SecretWord, M: LimbsViewConst, mBits: int) =

constantine/math_arbitrary_precision/arithmetic/limbs_divmod_vartime.nim
@@ -1,0 +1,228 @@
+# Constantine
+# Copyright (c) 2018-2019    Status Research & Development GmbH
+# Copyright (c) 2020-Present Mamy André-Ratsimbazafy
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at http://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at http://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+import
+  ../../platforms/abstractions,
+  ../../platforms/intrinsics/extended_precision_vartime,
+  ./limbs_views
+
+# No exceptions allowed
+{.push raises: [], checks: off.}
+
+# ############################################################
+#
+#              Division and Modular Reduction
+#                    (variable-time)
+#
+# ############################################################
+
+func shlAddMod_multiprec_vartime(
+       a: var openArray[SecretWord], c: SecretWord,
+       M: openArray[SecretWord], mBits: int): SecretWord {.meter.} =
+  ## Fused modular left-shift + add
+  ## Computes: a <- a shl 2ʷ + c (mod M)
+  ## Returns: (a shl 2ʷ + c) / M
+  ##
+  ## with w the base word width, usually 32 on 32-bit platforms and 64 on 64-bit platforms
+  ##
+  ## The modulus `M` most-significant bit at `mBits` MUST be set.
+  ##
+  ## Specialized for M being a multi-precision integer.
+                                        # Assuming 64-bit words
+  let hi = a[^1]                        # Save the high word to detect carries
+  let R = mBits and (WordBitWidth - 1)  # R = mBits mod 64
+
+  var a0, a1, m0: SecretWord
+  if R == 0:                            # If the number of mBits is a multiple of 64
+    a0 = a[^1]                          #
+    copyWords(a.view(), 1,              # we can just shift words
+              a.view(), 0, a.len-1)     #
+    a[0] = c                            # and replace the first one by c
+    a1 = a[^1]
+    m0 = M[^1]
+  else:                                 # Else: need to deal with partial word shifts at the edge.
+    let clz = WordBitWidth-R
+    a0 = (a[^1] shl clz) or (a[^2] shr R)
+    copyWords(a.view(), 1,
+              a.view(), 0, a.len-1)
+    a[0] = c
+    a1 = (a[^1] shl clz) or (a[^2] shr R)
+    m0 = (M[^1] shl clz) or (M[^2] shr R)
+
+  # m0 has its high bit set. (a0, a1)/m0 fits in a limb.
+  # Get a quotient q, at most we will be 2 iterations off
+  # from the true quotient
+  var q: SecretWord                     # Estimate quotient
+  if bool(a0 == m0):                    # if a_hi == divisor
+    q = MaxWord                         # quotient = MaxWord (0b1111...1111)
+  elif bool(a0.isZero()) and
+       bool(a1 < m0):                   # elif q == 0, true quotient = 0
+    q = Zero
+    return q
+  else:
+    var r: SecretWord
+    div2n1n_vartime(q, r, a0, a1, m0)   # else instead of being of by 0, 1 or 2
+    q -= One                            # we return q-1 to be off by -1, 0 or 1
+
+  # Now substract a*2^64 - q*m
+  var carry = Zero
+  var overM = true                      # Track if quotient greater than the modulus
+
+  for i in 0 ..< M.len:
+    var qm_lo: SecretWord
+    block:                              # q*m
+      # q * m + carry (doubleword) carry from previous limb
+      muladd1(carry, qm_lo, q, M[i], carry)
+
+    block:                              # a*2^64 - q*m
+      var borrow: Borrow
+      subB(borrow, a[i], a[i], qm_lo, Borrow(0))
+      carry += SecretWord(borrow) # Adjust if borrow
+
+    if bool(a[i] != M[i]):
+      overM = bool(a[i] > M[i])
+
+  # Fix quotient, the true quotient is either q-1, q or q+1
+  #
+  # if carry < q or carry == q and overM we must do "a -= M"
+  # if carry > hi (negative result) we must do "a += M"
+  if bool(carry > hi):
+    var c = Carry(0)
+    for i in 0 ..< a.len:
+      addC(c, a[i], a[i], M[i], c)
+    q -= One
+  elif overM or bool(carry < hi):
+    var b = Borrow(0)
+    for i in 0 ..< a.len:
+      subB(b, a[i], a[i], M[i], b)
+    q += One
+
+  return q
+
+func shortDiv_vartime*(remainder: var SecretWord, n_hi, n_lo, d: SecretWord, normFactor: int): SecretWord =
+  # We normalize d with clz so that the MSB is set
+  # And normalize (n_hi * 2^64 + n_lo) by normFactor as well to maintain the result
+  # This ensures that (n_hi, n_hi)/d fits in a limb.
+  if normFactor == 0:
+    div2n1n_vartime(result, remainder, n_hi, n_lo, d)
+  else:
+    let clz = WordBitWidth-normFactor
+    let hi = (n_hi shl clz) or (n_lo shr normFactor)
+    let lo = n_lo shl clz
+    let d = d shl clz
+
+    div2n1n_vartime(result, remainder, hi, lo, d)
+    remainder = remainder shr clz
+
+func shlAddMod_vartime(a: var openArray[SecretWord], c: SecretWord,
+                       M: openArray[SecretWord], mBits: int): SecretWord {.meter.} =
+  ## Fused modular left-shift + add
+  ## Computes: a <- a shl 2ʷ + c (mod M)
+  ## Returns: (a shl 2ʷ + c) / M
+  ##
+  ## with w the base word width, usually 32 on 32-bit platforms and 64 on 64-bit platforms
+  ##
+  ## The modulus `M` most-significant bit at `mBits` MUST be set.
+  if mBits <= WordBitWidth:
+    # If M fits in a single limb
+
+    # We normalize M with clz so that the MSB is set
+    # And normalize (a * 2^64 + c) by R as well to maintain the result
+    # This ensures that (a0, a1)/m0 fits in a limb.
+    let R = mBits and (WordBitWidth - 1)
+
+    # (hi, lo) = a * 2^64 + c
+    return shortDiv_vartime(remainder = a[0], n_hi = a[0], n_lo = c, d = M[0], normFactor = R)
+  else:
+    return shlAddMod_multiprec_vartime(a, c, M, mBits)
+
+func divRem_vartime*(
+       q, r: var openArray[SecretWord],
+       a, b: openArray[SecretWord]): bool {.meter.} =
+  # q = a div b
+  # r = a mod b
+  #
+  # Requirements:
+  # b != 0
+  # q.len > a.len - b.len
+  # r.len >= b.len
+
+  let aBits = getBits_LE_vartime(a)
+  let bBits = getBits_LE_vartime(b)
+  let aLen = wordsRequired(aBits)
+  let bLen = wordsRequired(bBits)
+  let rLen = bLen
+
+  let aOffset = a.len - b.len
+
+  # Note: don't confuse a.len and aLen (actually used words)
+
+  if unlikely(bool(r.len < bLen)):
+    # remainder buffer cannot store up to modulus size
+    return false
+
+  if unlikely(bool(q.len < aOffset+1)):
+    # quotient buffer cannot store up to quotient size
+    return false
+
+  if unlikely(bBits == 0):
+    # Divide by zero
+    return false
+
+  if aBits < bBits:
+    # if a uses less bits than b,
+    # a < b, so q = 0 and r = a
+    copyWords(r.view(), 0, a.view(), 0, aLen)
+    for i in aLen ..< r.len:
+      r[i] = Zero
+    for i in 0 ..< q.len:
+      q[i] = Zero
+  else:
+    # The length of a is at least the divisor
+    # We can copy bLen-1 words
+    # and modular shift-left-add the rest
+
+    copyWords(r.view(), 0, a.view(), aOffset+1, b.len-1)
+    r[rLen-1] = Zero
+    # Now shift-left the copied words while adding the new word mod b
+
+    for i in countdown(aOffset, 0):
+      q[i] = shlAddMod_vartime(
+              r.toOpenArray(0, rLen-1),
+              a[i],
+              b.toOpenArray(0, bLen-1),
+              bBits)
+
+    # Clean up extra words
+    for i in aOffset+1 ..< q.len:
+      q[i] = Zero
+    for i in rLen ..< r.len:
+      r[i] = Zero
+
+  return true
+
+func reduce_vartime*(r: var openArray[SecretWord],
+                     a, b: openArray[SecretWord]): bool {.noInline, meter.} =
+  let aOffset = max(a.len - b.len, 0)
+  var qBuf = allocStackArray(SecretWord, aOffset+1)
+  template q: untyped = qBuf.toOpenArray(0, aOffset)
+  result = divRem_vartime(q, r, a, b)
+
+# ############################################################
+#
+#                    Barrett Reduction
+#
+# ############################################################
+
+# - https://en.wikipedia.org/wiki/Barrett_reduction
+# - Handbook of Applied Cryptography
+#   Alfred J. Menezes, Paul C. van Oorschot and Scott A. Vanstone
+#   https://cacr.uwaterloo.ca/hac/about/chap14.pdf
+# - Modern Computer Arithmetic
+#   Richard P. Brent and Paul Zimmermann
+#   https://members.loria.fr/PZimmermann/mca/mca-cup-0.5.9.pdf

constantine/math_arbitrary_precision/arithmetic/limbs_extmul.nim
@@ -8,7 +8,8 @@
 
 import
   # Internal
-  ../../platforms/[abstractions, allocs]
+  ../../platforms/[abstractions, allocs],
+  ./limbs_views
 
 func prod_comba(r: var openArray[SecretWord], a, b: openArray[SecretWord]) {.noInline, tags: [Alloca].} =
   ## Extended precision multiplication
@@ -42,3 +43,62 @@ func prod_comba(r: var openArray[SecretWord], a, b: openArray[SecretWord]) {.noI
 func prod*(r: var openArray[SecretWord], a, b: openArray[SecretWord]) {.inline, meter.}=
   ## Extended precision multiplication
   r.prod_comba(a, b)
+
+func prod_vartime*(r: var openArray[SecretWord], a, b: openArray[SecretWord]) {.inline, meter.}=
+  ## Extended precision multiplication (vartime)
+
+  let aBits  = a.getBits_LE_vartime()
+  let bBits  = b.getBits_LE_vartime()
+
+  let aWords = wordsRequired(aBits)
+  let bWords = wordsRequired(bBits)
+
+  r.prod_comba(
+    a.toOpenArray(0, aWords-1),
+    b.toOpenArray(0, bWords-1))
+
+func square_comba(r: var openArray[SecretWord], a: openArray[SecretWord]) {.noInline, tags: [Alloca].} =
+  ## Extended precision squaring
+  # We use Product Scanning / Comba multiplication
+  var t, u, v = Zero
+  let stopEx = min(a.len * 2, r.len)
+
+  let tmp = allocStackArray(SecretWord, stopEx)
+
+  for i in 0 ..< stopEx:
+    # Invariant for product scanning:
+    # if we multiply accumulate by a[k1] * b[k2]
+    # we have k1+k2 == i
+    let ib = min(a.len-1, i)
+    let ia = i - ib
+    for j in  0 ..< min(a.len - ia, ib+1):
+      let k1 = ia+j
+      let k2 = ib-j
+      if k1 < k2:
+        mulDoubleAcc(t, u, v, a[k1], a[k2])
+      elif k1 == k2:
+        mulAcc(t, u, v, a[k1], a[k2])
+      else:
+        discard
+
+    tmp[i] = v
+    if i < stopEx-1:
+      v = u
+      u = t
+      t = Zero
+
+  for i in 0 ..< stopEx:
+    r[i] = tmp[i]
+
+  for i in stopEx ..< r.len:
+    r[i] = Zero
+
+func square*(r: var openArray[SecretWord], a: openArray[SecretWord]) {.inline, meter.}=
+  ## Extended precision squaring
+  r.square_comba(a)
+
+func square_vartime*(r: var openArray[SecretWord], a: openArray[SecretWord]) {.inline, meter.}=
+  ## Extended precision squaring (vartime)
+  let aBits  = a.getBits_LE_vartime()
+  let aWords = wordsRequired(aBits)
+  r.square_comba(a.toOpenArray(0, aWords-1))

commit 12b6ce5ca4d095a8f18f4a384bb0b37053fc6a0c

fix the new div2n1n_vartime on 32-bit - regression from #286

constantine/platforms/intrinsics/extended_precision_vartime.nim
@@ -16,7 +16,7 @@
 import ../abstractions
 
 func div2n1n_nim_vartime[T: SomeUnsignedInt](q, r: var T, n_hi, n_lo, d: T) {.tags:[VarTime].}=
-  ## Division uint128 by uint64
+  ## Division uint128 by uint64 or uint64 by uint32
   ## Warning ⚠️ :
   ##   - if n_hi == d, quotient does not fit in an uint64 and will throw SIGFPE
   ##   - if n_hi > d result is undefined
@@ -108,15 +108,16 @@ func div2n1n_vartime*(q, r: var SecretWord, n_hi, n_lo, d: SecretWord) {.inline.
   ## To avoid issues, n_hi, n_lo, d should be normalized.
   ## i.e. shifted (== multiplied by the same power of 2)
   ## so that the most significant bit in d is set.
-  when sizeof(int) == 4:
+  when sizeof(int) == 4 or defined(CTT_32):
     let dividend = (uint64(n_hi) shl 32) or uint64(n_lo)
     let divisor = uint64(d)
-    q = uint32(dividend div divisor)
-    r = uint32(dividend mod divisor)
-  when nimvm:
-    div2n1n_nim_vartime(BaseType q, BaseType r, BaseType n_hi, BaseType n_lo, BaseType d)
+    q = SecretWord(dividend div divisor)
+    r = SecretWord(dividend mod divisor)
   else:
-    when declared(div2n1n_128_vartime):
-      div2n1n_128_vartime(BaseType q, BaseType r, BaseType n_hi, BaseType n_lo, BaseType d)
-    else:
+    when nimvm:
       div2n1n_nim_vartime(BaseType q, BaseType r, BaseType n_hi, BaseType n_lo, BaseType d)
+    else:
+      when declared(div2n1n_128_vartime):
+        div2n1n_128_vartime(BaseType q, BaseType r, BaseType n_hi, BaseType n_lo, BaseType d)
+      else:
+        div2n1n_nim_vartime(BaseType q, BaseType r, BaseType n_hi, BaseType n_lo, BaseType d)

tests/math_arbitrary_precision/t_bigints_mod.nim
@@ -4,7 +4,8 @@ import
   ../../constantine/math/[
     arithmetic,
     io/io_bigints],
-  ../../constantine/math_arbitrary_precision/arithmetic/limbs_divmod_vartime
+  ../../constantine/math_arbitrary_precision/arithmetic/limbs_divmod_vartime,
+  ../../constantine/platforms/abstractions
 
 let a = BigInt[64].fromUint(0xa0e5cb56a1c08396'u64)
 let M = BigInt[64].fromUint(0xae57180eceb0206f'u64)
@@ -14,9 +15,9 @@ var r, r2: BigInt[64]
 r.reduce(a, M)
 doAssert r2.limbs.reduce_vartime(a.limbs, M.limbs)
 
-let rU64 = 0xa0e5cb56a1c08396'u64 mod 0xae57180eceb0206f'u64
+let rBase = cast[BaseType](0xa0e5cb56a1c08396'u64) mod cast[BaseType](0xae57180eceb0206f'u64)
 # echo r.toHex()
 
-doAssert rU64 == a.limbs[0].uint64
+doAssert rBase == a.limbs[0].BaseType
 doAssert bool(a == r)
 echo "SUCCESS: t_bigints_mod.nim"