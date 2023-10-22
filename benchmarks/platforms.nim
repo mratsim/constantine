50e759313a95278481be702fc0dd7e04789cf54e
Mamy Ratsimbazafy <mamy_github@numforge.co>
2023-10-22 02:20:35 +0200

    remove duplicate getTicks from benchmarks folder

--- benchmarks/platforms.nim
+++ benchmarks/platforms.nim
@@ -6,25 +6,5 @@
 #   * Apache v2 license (license terms in the root directory or at http://www.apache.org/licenses/LICENSE-2.0).
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-when defined(amd64): # TODO defined(i386) but it seems like RDTSC call is misconfigured
-  import ../constantine/platforms/metering/benchmarking
-  export getTicks, cpuName
-
-  const SupportsCPUName* = true
-  const SupportsGetTicks* = true
-else:
-  const SupportsCPUName* = false
-  const SupportsGetTicks* = false
-
-# Prevent compiler optimizing benchmark away
-# -----------------------------------------------
-# This doesn't always work unfortunately ...
-
-proc volatilize(x: ptr byte) {.codegenDecl: "$# $#(char const volatile *x)", inline.} =
-  discard
-
-template preventOptimAway*[T](x: var T) =
-  volatilize(cast[ptr byte](unsafeAddr x))
-
-template preventOptimAway*[T](x: T) =
-  volatilize(cast[ptr byte](x))
+import ../constantine/platforms/metering/benchmarking
+export benchmarking
