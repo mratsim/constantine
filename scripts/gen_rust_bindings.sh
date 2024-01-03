3ed6eeac4f0a9b0092bb4234b61768dfde25cdbe
Mamy Ratsimbazafy <mamy_github@numforge.co>
2024-01-03 10:54:48 +0100

    constantine-rust: public fields and Default for https://github.com/sifraitech/rust-kzg

--- scripts/gen_rust_bindings.sh
+++ scripts/gen_rust_bindings.sh
@@ -25,11 +25,15 @@ bindgen \
   --with-derive-custom-enum ".*?_status"=Debug \
   --with-derive-custom-enum ".*?_format"=Debug \
   --with-derive-custom-struct ctt_threadpool=Debug \
-  --default-visibility private \
+  --with-derive-default \
   --enable-function-attribute-detection \
   -- -Iinclude
 
 # --must-use-type ".*?_status" is not needed with function attribute detection
+#
+# Note: this is a constantine-rust built with public fields for https://github.com/sifraitech/rust-kzg
+#   Removed: --default-visibility private \
+#   Added:   --with-derive-default \
 
 bindgen \
   include/constantine.h \
@@ -41,6 +45,6 @@ bindgen \
   --with-derive-custom-enum ".*?_status"=Debug \
   --with-derive-custom-enum ".*?_format"=Debug \
   --with-derive-custom-struct ctt_threadpool=Debug \
-  --default-visibility private \
+  --with-derive-default \
   --enable-function-attribute-detection \
   -- -m32 -Iinclude
