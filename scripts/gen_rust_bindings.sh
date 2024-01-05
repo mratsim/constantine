944568336b013794b6b84f544d6c6a0511ae6603
Mamy Ratsimbazafy <mamy_github@numforge.co>
2024-01-05 01:46:41 +0100

    bindings: Rust supports 32-bit platforms

--- scripts/gen_rust_bindings.sh
+++ scripts/gen_rust_bindings.sh
@@ -17,7 +17,7 @@
 
 bindgen \
   include/constantine.h \
-  -o constantine-rust/constantine-sys/src/bindings.rs \
+  -o constantine-rust/constantine-sys/src/bindings64.rs \
   --default-enum-style rust \
   --use-core \
   --no-copy ctt_threadpool \
@@ -30,3 +30,17 @@ bindgen \
   -- -Iinclude
 
 # --must-use-type ".*?_status" is not needed with function attribute detection
+
+bindgen \
+  include/constantine.h \
+  -o constantine-rust/constantine-sys/src/bindings32.rs \
+  --default-enum-style rust \
+  --use-core \
+  --no-copy ctt_threadpool \
+  --no-derive-debug \
+  --with-derive-custom-enum ".*?_status"=Debug \
+  --with-derive-custom-enum ".*?_format"=Debug \
+  --with-derive-custom-struct ctt_threadpool=Debug \
+  --default-visibility private \
+  --enable-function-attribute-detection \
+  -- -m32 -Iinclude
