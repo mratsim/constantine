e84b6136bfefcd62c7a196b635f21ee4dd23c49b
Agnish Ghosh <iamagnix@gmail.com>
2024-01-09 03:14:10 +0530

    add: vector commitment test, crs fix and consistency check

--- constantine/eth_verkle_ipa/transcript_gen.nim
+++ constantine/eth_verkle_ipa/transcript_gen.nim
@@ -13,52 +13,51 @@
 # ############################################################
 
 import
-    ./eth_verkle_constants,
-    ../platforms/[primitives,abstractions],
-    ../serialization/endians,
-    ../math/config/[type_ff, curves],
-    ../math/[extension_fields, arithmetic],
-    ../math/elliptic/ec_twistededwards_projective,
-    ../hashes,
-    ../serialization/[codecs_banderwagon,codecs_status_codes]
+  ./eth_verkle_constants,
+  ../platforms/[primitives,abstractions],
+  ../serialization/endians,
+  ../math/config/[type_ff, curves],
+  ../math/[extension_fields, arithmetic],
+  ../math/elliptic/ec_twistededwards_projective,
+  ../hashes,
+  ../serialization/[codecs_banderwagon,codecs_status_codes]
 
 func newTranscriptGen*(res: var CryptoHash, label: openArray[byte]) =
-    res.init()
-    res.update(label)
+  res.init()
+  res.update(label)
 
-func messageAppend*( res: var CryptoHash, message: openArray[byte], label: openArray[byte]) =
-    res.init()
-    res.update(label)
-    res.update(message)
+func messageAppend*(res: var CryptoHash, message: openArray[byte], label: openArray[byte]) =
+  res.init()
+  res.update(label)
+  res.update(message)
 
 func messageAppend_u64*(res: var CryptoHash, label: openArray[byte], num_value: uint64) = 
-    res.init()
-    res.update(label)
-    res.update(num_value.toBytes(bigEndian))
+  res.init()
+  res.update(label)
+  res.update(num_value.toBytes(bigEndian))
 
 func domainSeparator*(res: var CryptoHash, label: openArray[byte]) =
-    var state {.noInit.} : CryptoHash
-    state.update(label)
+  var state {.noInit.} : CryptoHash
+  state.update(label)
 
 func pointAppend*(res: var CryptoHash, label: openArray[byte], point: EC_P) =
-    var bytes {.noInit.}: array[32, byte]
-    if(bytes.serialize(point) == cttCodecEcc_Success):
-        res.messageAppend(bytes, label)
+  var bytes {.noInit.}: array[32, byte]
+  if bytes.serialize(point) == cttCodecEcc_Success:
+    res.messageAppend(bytes, label)
 
 func scalarAppend*(res: var CryptoHash, label: openArray[byte], scalar: matchingOrderBigInt(Banderwagon)) =
-    var bytes {.noInit.}: array[32, byte]
+  var bytes {.noInit.}: array[32, byte]
 
-    if(bytes.serialize_scalar(scalar) == cttCodecScalar_Success):
-        res.messageAppend(bytes, label)
-
+  if bytes.serialize_scalar(scalar) == cttCodecScalar_Success:
+    res.messageAppend(bytes, label)
 
 func generateChallengeScalar*(gen: var matchingOrderBigInt(Banderwagon), transcript: var CryptoHash, label: openArray[byte]) =
-    # Generating Challenge Scalars based on the Fiat Shamir method
-    transcript.domainSeparator(label)
+  # Generating Challenge Scalars based on the Fiat Shamir method
+  transcript.domainSeparator(label)
 
-    var hash: array[32, byte]
-    transcript.finish(hash)
+  var hash: array[32, byte]
+  transcript.finish(hash)
 
-    if(gen.deserialize_scalar(hash) == cttCodecScalar_Success):
-        transcript.clear()
-        transcript.scalarAppend(label, gen)
+  if gen.deserialize_scalar(hash) == cttCodecScalar_Success:
+    transcript.clear()
+    transcript.scalarAppend(label, gen)
