da576196713843adbdaa6781a192182822e30ddf
Vindaar <basti90@gmail.com>
2024-05-30 17:47:28 +0200

    add Display trait to `ctt_eth_*` enums
    NOTE: I'm not sure right now whether `lib.rs` is *also* generated by
    `bindgen` or not? I would guess not? Because in that case this change
    wouldn't be a good idea obviously (overwritten on next wrapper update)

--- constantine-rust/constantine-sys/src/lib.rs
+++ constantine-rust/constantine-sys/src/lib.rs
@@ -6,3 +6,44 @@
 include!("bindings64.rs");
 #[cfg(target_pointer_width = "32")]
 include!("bindings32.rs");
+
+impl std::fmt::Display for ctt_eth_bls_status {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let s = match self {
+            ctt_eth_bls_status::cttEthBls_Success => "cttEthBls_Success",
+            ctt_eth_bls_status::cttEthBls_VerificationFailure => "cttEthBls_VerificationFailure",
+            ctt_eth_bls_status::cttEthBls_InputsLengthsMismatch => "cttEthBls_InputsLengthsMismatch",
+            ctt_eth_bls_status::cttEthBls_ZeroLengthAggregation => "cttEthBls_ZeroLengthAggregation",
+            ctt_eth_bls_status::cttEthBls_PointAtInfinity => "cttEthBls_PointAtInfinity",
+            _ => "cttEthBls_InvalidStatusCode",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+impl std::fmt::Display for ctt_codec_scalar_status {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let s = match self {
+            ctt_codec_scalar_status::cttCodecScalar_Success => "cttCodecScalar_Success",
+            ctt_codec_scalar_status::cttCodecScalar_Zero => "cttCodecScalar_Zero",
+            ctt_codec_scalar_status::cttCodecScalar_ScalarLargerThanCurveOrder => "cttCodecScalar_ScalarLargerThanCurveOrder",
+            _ => "cttCodecScalar_InvalidStatusCode",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+impl std::fmt::Display for ctt_codec_ecc_status {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let s = match self {
+            ctt_codec_ecc_status::cttCodecEcc_Success => "cttCodecEcc_Success",
+            ctt_codec_ecc_status::cttCodecEcc_InvalidEncoding => "cttCodecEcc_Zero",
+            ctt_codec_ecc_status::cttCodecEcc_CoordinateGreaterThanOrEqualModulus => "cttCodecEcc_CoordinateGreaterThanOrEqualModulus",
+            ctt_codec_ecc_status::cttCodecEcc_PointNotOnCurve => "cttCodecEcc_PointNotOnCurve",
+            ctt_codec_ecc_status::cttCodecEcc_PointNotInSubgroup => "cttCodecEcc_PointNotInSubgroup",
+            ctt_codec_ecc_status::cttCodecEcc_PointAtInfinity => "cttCodecEcc_PointAtInfinity",
+            _ => "cttCodecEcc_InvalidStatusCode",
+        };
+        write!(f, "{}", s)
+    }
+}
