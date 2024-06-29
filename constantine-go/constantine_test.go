daaec82a3a1212af953c70094148208ee878667f
Vindaar <basti90@gmail.com>
2024-06-29 14:03:01 +0200

    [Go] update Go EVM precompiles test for new API
    NOTE: currently not all tests pass, due to the unmarshaling bug. We
    will rebase once a final decision for the solution has been made.

--- constantine-go/constantine_test.go
+++ constantine-go/constantine_test.go
@@ -1423,151 +1423,288 @@ type PrecompileTest struct {
 	NoBenchmark bool
 }
 
-func loadVectors(fname string) (result []PrecompileTest) {
+func loadVectors(fname string) (result []PrecompileTest, status bool) {
 	var test []PrecompileTest
 
 	testFile, err := os.Open(fname)
 	err = json.NewDecoder(testFile).Decode(&test)
 	if err != nil {
-		fmt.Println("hehe")
+		return test, false
 	}
 
-	return test
+	return test, true
 }
 
-type TestFunction func([]byte, []byte) (bool, error)
+func parseTest(vec PrecompileTest) (inputBytes []byte, expectedBytes []byte, success bool) {
+	input := vec.Input
+	expected := vec.Expected
+
+	inputBytes = make([]byte, len(input)/2, len(input)/2)
+	expectedBytes = make([]byte, len(expected)/2, len(expected)/2)
+
+	err := fromHexImpl(inputBytes[:], []byte(input))
+	if err != nil {
+		return inputBytes, expectedBytes, false
+	}
+	err = fromHexImpl(expectedBytes[:], []byte(expected))
+	if err != nil {
+		return inputBytes, expectedBytes, false
+	}
+
+	return inputBytes, expectedBytes, true
+}
+
+type TF func([]byte) ([]byte, error)
+type TF32 func([]byte) (Bytes32, error)
+type TF64 func([]byte) (Bytes64, error)
+type TF128 func([]byte) (Bytes128, error)
+type TF256 func([]byte) (Bytes256, error)
 
 // Helper function to simplify the test generation. No need to duplicate test logic, all the same
-func runTest(t *testing.T, testPath string, fn TestFunction) {
+func runTestSlice(t *testing.T, testPath string, fn TF) {
 	fmt.Println("Running test for path: ", testPath)
 	tests, _ := filepath.Glob(testPath)
 	for _, testPath := range tests {
 		t.Run(testPath, func(t *testing.T) {
 			// Load from the given path
-			vectors := loadVectors(testPath)
+			vectors, pStatus := loadVectors(testPath)
+			if !pStatus {
+				require.True(t, false)
+			}
 			for _, vec := range vectors {
 				fmt.Println("Running test case: ", vec.Name)
 
-				input := vec.Input
-				expected := vec.Expected
-
-				var inputBytes []byte
-				inputBytes = make([]byte, len(input)/2, len(input)/2)
-				var expectedBytes []byte
-				expectedBytes = make([]byte, len(expected)/2, len(expected)/2)
-
-				err := fromHexImpl(inputBytes[:], []byte(input))
-				if err != nil {
+				inputBytes, expectedBytes, status := parseTest(vec)
+				if !status {
 					require.True(t, false)
 				}
-				err = fromHexImpl(expectedBytes[:], []byte(expected))
-				if err != nil {
-					require.True(t, false)
-				}
-
-				r := make([]byte, len(expected)/2, len(expected)/2)
 
 				// Call the test function
-				status, err := fn(r, inputBytes)
-				if status != true {
-					for i := range r { // reset to 0 (expected might be an empty array)
-						r[i] = '0'
-					}
+				r, err := fn(inputBytes)
+				if err != nil {
+					// in this case expected should be empty
+					require.Equal(t, expectedBytes[:], r[:])
+				} else {
+					require.Equal(t, expectedBytes[:], r[:])
 				}
-				require.Equal(t, r[:], expectedBytes[:])
+			}
+		})
+	}
+}
+
+func runTestB32(t *testing.T, testPath string, fn TF32) {
+	fmt.Println("Running test for path: ", testPath)
+	tests, _ := filepath.Glob(testPath)
+	for _, testPath := range tests {
+		t.Run(testPath, func(t *testing.T) {
+			// Load from the given path
+			vectors, pStatus := loadVectors(testPath)
+			if !pStatus {
+				require.True(t, false)
+			}
+			for _, vec := range vectors {
+				fmt.Println("Running test case: ", vec.Name)
+
+				inputBytes, expectedBytes, status := parseTest(vec)
+				if !status {
+					require.True(t, false)
+				}
+
+				// Call the test function
+				r, err := fn(inputBytes)
+				if err != nil {
+					// in this case expected should be empty
+					require.Equal(t, 0, len(expectedBytes))
+				} else {
+					require.Equal(t, expectedBytes[:], r[:])
+				}
+			}
+		})
+	}
+}
+
+func runTestB64(t *testing.T, testPath string, fn TF64) {
+	fmt.Println("Running test for path: ", testPath)
+	tests, _ := filepath.Glob(testPath)
+	for _, testPath := range tests {
+		t.Run(testPath, func(t *testing.T) {
+			// Load from the given path
+			vectors, pStatus := loadVectors(testPath)
+			if !pStatus {
+				require.True(t, false)
+			}
+			for _, vec := range vectors {
+				fmt.Println("Running test case: ", vec.Name)
+
+				inputBytes, expectedBytes, status := parseTest(vec)
+				if !status {
+					require.True(t, false)
+				}
+
+
+				// Call the test function
+				r, err := fn(inputBytes)
+				if err != nil {
+					// in this case expected should be empty
+					require.Equal(t, 0, len(expectedBytes))
+				} else {
+					require.Equal(t, expectedBytes[:], r[:])
+				}
+			}
+		})
+	}
+}
+
+func runTestB128(t *testing.T, testPath string, fn TF128) {
+	fmt.Println("Running test for path: ", testPath)
+	tests, _ := filepath.Glob(testPath)
+	for _, testPath := range tests {
+		t.Run(testPath, func(t *testing.T) {
+			// Load from the given path
+			vectors, pStatus := loadVectors(testPath)
+			if !pStatus {
+				require.True(t, false)
+			}
+			for _, vec := range vectors {
+				fmt.Println("Running test case: ", vec.Name)
+
+				inputBytes, expectedBytes, status := parseTest(vec)
+				if !status {
+					require.True(t, false)
+				}
+
+				// Call the test function
+				r, err := fn(inputBytes)
+				if err != nil {
+					// in this case expected should be empty
+					require.Equal(t, 0, len(expectedBytes))
+				} else {
+					require.Equal(t, expectedBytes[:], r[:])
+				}
+			}
+		})
+	}
+}
+
+func runTestB256(t *testing.T, testPath string, fn TF256) {
+	fmt.Println("Running test for path: ", testPath)
+	tests, _ := filepath.Glob(testPath)
+	for _, testPath := range tests {
+		t.Run(testPath, func(t *testing.T) {
+			// Load from the given path
+			vectors, pStatus := loadVectors(testPath)
+			if !pStatus {
+				require.True(t, false)
+			}
+			for _, vec := range vectors {
+				fmt.Println("Running test case: ", vec.Name)
+
+				inputBytes, expectedBytes, status := parseTest(vec)
+				if !status {
+					require.True(t, false)
+				}
+
+				// Call the test function
+				r, err := fn(inputBytes)
+				if err != nil {
+					// in this case expected should be empty
+					require.Equal(t, 0, len(expectedBytes))
+				} else {
+					require.Equal(t, expectedBytes[:], r[:])
+				}
 			}
 		})
 	}
 }
 
 func TestModexp(t *testing.T) {
-	runTest(t, modexp_tests, EvmModexp)
+	runTestSlice(t, modexp_tests, EvmModexp)
 }
 
 func TestModexpEip2565(t *testing.T) {
-	runTest(t, modexp_eip2565_tests, EvmModexp)
+	runTestSlice(t, modexp_eip2565_tests, EvmModexp)
 }
 
 func TestBn256Add(t *testing.T) {
-	runTest(t, bn256Add_tests, EvmBn254G1Add)
+	runTestB64(t, bn256Add_tests, EvmBn254G1Add)
 }
 
 func TestBn256ScalarMul(t *testing.T) {
-	runTest(t, bn256ScalarMul_tests, EvmBn254G1Mul)
+	runTestB64(t, bn256ScalarMul_tests, EvmBn254G1Mul)
 }
 
 func TestBn256Pairing(t *testing.T) {
-	runTest(t, bn256Pairing_tests, EvmBn254G1EcPairingCheck)
+	runTestB32(t, bn256Pairing_tests, EvmBn254G1EcPairingCheck)
 }
 
 func TestAddG1Bls(t *testing.T) {
-	runTest(t, add_G1_bls_tests, EvmBls12381G1Add)
+	runTestB128(t, add_G1_bls_tests, EvmBls12381G1Add)
 }
 
 func TestFailAddG1Bls(t *testing.T) {
-	runTest(t, fail_add_G1_bls_tests, EvmBls12381G1Add)
+	runTestB128(t, fail_add_G1_bls_tests, EvmBls12381G1Add)
 }
 
 func TestAddG2Bls(t *testing.T) {
-	runTest(t, add_G2_bls_tests, EvmBls12381G2Add)
+	runTestB256(t, add_G2_bls_tests, EvmBls12381G2Add)
 }
 
 func TestFailAddG2Bls(t *testing.T) {
-	runTest(t, fail_add_G2_bls_tests, EvmBls12381G2Add)
+	runTestB256(t, fail_add_G2_bls_tests, EvmBls12381G2Add)
 }
 
 func TestMulG1Bls(t *testing.T) {
-	runTest(t, mul_G1_bls_tests, EvmBls12381G1Mul)
+	runTestB128(t, mul_G1_bls_tests, EvmBls12381G1Mul)
 }
 
 func TestFailMulG1Bls(t *testing.T) {
-	runTest(t, fail_mul_G1_bls_tests, EvmBls12381G1Mul)
+	runTestB128(t, fail_mul_G1_bls_tests, EvmBls12381G1Mul)
 }
 
 func TestMulG2Bls(t *testing.T) {
-	runTest(t, mul_G2_bls_tests, EvmBls12381G2Mul)
+	runTestB256(t, mul_G2_bls_tests, EvmBls12381G2Mul)
 }
 
 func TestFailMulG2Bls(t *testing.T) {
-	runTest(t, fail_mul_G2_bls_tests, EvmBls12381G2Mul)
+	runTestB256(t, fail_mul_G2_bls_tests, EvmBls12381G2Mul)
 }
 
 func TestMsmG1Bls(t *testing.T) {
-	runTest(t, multiexp_G1_bls_tests, EvmBls12381G1Msm)
+	runTestB128(t, multiexp_G1_bls_tests, EvmBls12381G1Msm)
 }
 
 func TestFailMsmG1Bls(t *testing.T) {
-	runTest(t, fail_multiexp_G1_bls_tests, EvmBls12381G1Msm)
+	runTestB128(t, fail_multiexp_G1_bls_tests, EvmBls12381G1Msm)
 }
 
 func TestMsmG2Bls(t *testing.T) {
-	runTest(t, multiexp_G2_bls_tests, EvmBls12381G2Msm)
+	runTestB256(t, multiexp_G2_bls_tests, EvmBls12381G2Msm)
 }
 
 func TestFailMsmG2Bls(t *testing.T) {
-	runTest(t, fail_multiexp_G2_bls_tests, EvmBls12381G2Msm)
+	runTestB256(t, fail_multiexp_G2_bls_tests, EvmBls12381G2Msm)
 }
 
 func TestPairingCheckBls(t *testing.T) {
-	runTest(t, pairing_check_bls_tests, EvmBls12381PairingCheck)
+	runTestB32(t, pairing_check_bls_tests, EvmBls12381PairingCheck)
 }
 
 func TestFailPairingCheckBls(t *testing.T) {
-	runTest(t, fail_pairing_check_bls_tests, EvmBls12381PairingCheck)
+	runTestB32(t, fail_pairing_check_bls_tests, EvmBls12381PairingCheck)
 }
 
 func TestMapFpToG1Bls(t *testing.T) {
-	runTest(t, map_fp_to_G1_bls_tests, EvmBls12381MapFpToG1)
+	runTestB128(t, map_fp_to_G1_bls_tests, EvmBls12381MapFpToG1)
 }
 
 func TestFailMapFpToG1Bls(t *testing.T) {
-	runTest(t, fail_map_fp_to_G1_bls_tests, EvmBls12381MapFpToG1)
+	runTestB128(t, fail_map_fp_to_G1_bls_tests, EvmBls12381MapFpToG1)
 }
 
 func TestMapFp2ToG2Bls(t *testing.T) {
-	runTest(t, map_fp2_to_G2_bls_tests, EvmBls12381MapFp2ToG2)
+	runTestB256(t, map_fp2_to_G2_bls_tests, EvmBls12381MapFp2ToG2)
 }
 
 func TestFailMapFp2ToG2Bls(t *testing.T) {
-	runTest(t, fail_map_fp2_to_G2_bls_tests, EvmBls12381MapFp2ToG2)
+	runTestB256(t, fail_map_fp2_to_G2_bls_tests, EvmBls12381MapFp2ToG2)
 }
