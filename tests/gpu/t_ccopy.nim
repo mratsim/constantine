3daef70685c741ebb44e5e5b8649f779acc2b5d1
Vindaar <basti90@gmail.com>
2024-09-23 16:58:23 +0200

    [tests] use NvidiaAssembler in Nvidia tests

--- tests/gpu/t_ccopy.nim
+++ tests/gpu/t_ccopy.nim
@@ -15,58 +15,6 @@ import
   constantine/platforms/llvm/llvm,
   constantine/math_compiler/[ir, pub_fields, codegen_nvidia]
 
-proc execCond*[T](jitFn: CUfunction, r: var T; a: T; c: SecretBool) =
-  ## Execute a binary operation in the form r <- op(a, b, c) with `c` a condition
-  ## on Nvidia GPU
-  # The execution wrapper provided are mostly for testing and debugging low-level kernels
-  # that serve as building blocks, like field addition or multiplication.
-  # They aren't parallelizable so we are not concern about the grid and block size.
-  # We also aren't concerned about the cuda stream when testing.
-  #
-  # This is not the case for production kernels (multi-scalar-multiplication, FFT)
-  # as we want to execute kernels asynchronously then merge results which might require multiple streams.
-
-  static: doAssert cpuEndian == littleEndian, block:
-    # From https://developer.nvidia.com/cuda-downloads?target_os=Linux
-    # Supported architectures for Cuda are:
-    # x86-64, PowerPC 64 little-endian, ARM64 (aarch64)
-    # which are all little-endian at word-level.
-    #
-    # Due to limbs being also stored in little-endian, on little-endian host
-    # the CPU and GPU will have the same binary representation
-    # whether we use 32-bit or 64-bit words, so naive memcpy can be used for parameter passing.
-
-    "Most CPUs (x86-64, ARM) are little-endian, as are Nvidia GPUs, which allows naive copying of parameters.\n" &
-    "Your architecture '" & $hostCPU & "' is big-endian and GPU offloading is unsupported on it."
-
-  # We assume that all arguments are passed by reference in the Cuda kernel, hence the need for GPU alloc.
-
-  var rGPU, aGPU, bGPU, cGPU: CUdeviceptr
-  check cuMemAlloc(rGPU, csize_t sizeof(r))
-  check cuMemAlloc(aGPU, csize_t sizeof(a))
-
-  echo "The secret bool is ? ", c.bool
-  check cuMemcpyHtoD(rGPU, r.addr, csize_t sizeof(r))
-  check cuMemcpyHtoD(aGPU, a.addr, csize_t sizeof(a))
-
-  let params = [pointer(rGPU.addr), pointer(aGPU.addr), pointer(c.addr)]
-
-  check cuLaunchKernel(
-          jitFn,
-          1, 1, 1, # grid(x, y, z)
-          1, 1, 1, # block(x, y, z)
-          sharedMemBytes = 0,
-          CUstream(nil),
-          params[0].unsafeAddr, nil)
-
-  check cuMemcpyDtoH(r.addr, rGPU, csize_t sizeof(r))
-
-  check cuMemFree(rGPU)
-  check cuMemFree(aGPU)
-  check cuMemFree(bGPU)
-  check cuMemFree(cGPU)
-
-
 block:
   var a = fromHex(BigInt[128], "0x12345678FF11FFAA00321321CAFECAFE")
   let b = fromHex(BigInt[128], "0xDEADBEEFDEADBEEFDEADBEEFDEADBEEF")
@@ -85,47 +33,11 @@ block:
 
   doAssert bool(expected == b)
 
-## XXX: Add `ccopy` check for a finite field!
-
-# Init LLVM
-# -------------------------
-initializeFullNVPTXTarget()
-
-# Init GPU
-# -------------------------
-let cudaDevice = cudaDeviceInit()
-var sm: tuple[major, minor: int32]
-check cuDeviceGetAttribute(sm.major, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR, cudaDevice)
-check cuDeviceGetAttribute(sm.minor, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR, cudaDevice)
-
 proc testName[Name: static Algebra](field: type FF[Name], wordSize: int, a, b: FF[Name]) =
   # Codegen
   # -------------------------
-  let name = if field is Fp: $Name & "_fp"
-             else: $Name & "_fr"
-  let asy = Assembler_LLVM.new(bkNvidiaPTX, cstring("t_nvidia_" & name & $wordSize))
-  let fd = asy.ctx.configureField(
-    name, field.bits(),
-    field.getModulus().toHex(),
-    v = 1, w = wordSize
-  )
-
-  asy.definePrimitives(fd)
-
-  let kernName = asy.genFpCcopy(fd)
-  let ptx = asy.codegenNvidiaPTX(sm)
-
-  # GPU exec
-  # -------------------------
-  var cuCtx: CUcontext
-  var cuMod: CUmodule
-  check cuCtxCreate(cuCtx, 0, cudaDevice)
-  check cuModuleLoadData(cuMod, ptx)
-  defer:
-    check cuMod.cuModuleUnload()
-    check cuCtx.cuCtxDestroy()
-
-  let kernel = cuMod.getCudaKernel(kernName)
+  let nv = initNvAsm(field, wordSize)
+  let kernel = nv.compile(genFpCcopy)
 
   template executeCompare(cond): untyped {.dirty.} =
     var rCPU, rGPU: field
@@ -134,16 +46,17 @@ proc testName[Name: static Algebra](field: type FF[Name], wordSize: int, a, b: F
 
     rCPU.ccopy(b, SecretBool(cond))
 
-    kernel.execCond(rGPU, b, cond)
+    let c = cond # needs to be a runtime value so that it has an address!
+    kernel.execCuda(res = rGPU, inputs = (b, c))
 
-    echo rCPU.toHex()
-    echo rGPU.toHex()
+    echo "CPU = ", rCPU.toHex()
+    echo "GPU = ", rGPU.toHex()
     doAssert bool(rCPU == rGPU)
 
   block True:
-    executeCompare(CtTrue)
+    executeCompare(true)
   block False:
-    executeCompare(CtFalse)
+    executeCompare(false)
 
 
 let a = Fp[BN254_Snarks].fromHex("0x12345678FF11FFAA00321321CAFECAFE")
