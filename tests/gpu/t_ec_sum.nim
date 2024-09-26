c74af7417b1928b5f1aa1b79932950cb5cc5e320
Vindaar <basti90@gmail.com>
2024-09-26 13:58:13 +0200

    [tests] add test case for jacobian + affine coords

--- tests/gpu/t_ec_sum.nim
+++ tests/gpu/t_ec_sum.nim
@@ -11,15 +11,15 @@ import
   constantine/named/algebras,
   constantine/math/io/[io_bigints, io_fields, io_ec],
   constantine/math/arithmetic,
-  constantine/math/elliptic/ec_shortweierstrass_jacobian,
+  constantine/math/elliptic/[ec_shortweierstrass_jacobian, ec_shortweierstrass_affine],
   constantine/platforms/abstractions,
   constantine/platforms/llvm/llvm,
   constantine/math_compiler/[ir, pub_fields, pub_curves_jacobian, codegen_nvidia, impl_fields_globals],
   # Test utilities
   helpers/prng_unsafe
 
-proc test[Name: static Algebra](field: type FF[Name], wordSize: int,
-                                a, b: EC_ShortW_Jac[field, G1]) =
+proc testSum[Name: static Algebra](field: type FF[Name], wordSize: int,
+                                   a, b: EC_ShortW_Jac[field, G1]) =
   # Codegen
   # -------------------------
   let nv = initNvAsm(EC_ShortW_Jac[field, G1], wordSize)
@@ -32,7 +32,7 @@ proc test[Name: static Algebra](field: type FF[Name], wordSize: int,
 
     # For GPU:
     var rGPU: EC_ShortW_Jac[field, G1]
-    kernel.exec(rGPU, a, b)
+    kernel.execCuda(res = rGPU, inputs = (a, b))
 
     # Verify CPU and GPU agree
     doAssert bool(rCPU == rGPU)
@@ -43,6 +43,32 @@ proc test[Name: static Algebra](field: type FF[Name], wordSize: int,
   for i in 0 ..< 1000: # `res = res + b`, starting with `a + b`
     res = checkSum(res, b)
 
+proc testMixedSum[Name: static Algebra](field: type FF[Name], wordSize: int,
+                                        a: EC_ShortW_Jac[field, G1], b: EC_ShortW_Aff[field, G1]) =
+  # Codegen
+  # -------------------------
+  let nv = initNvAsm(EC_ShortW_Jac[field, G1], wordSize)
+  let kernel = nv.compile(genEcMixedSum)
+
+  template checkSum(a, b): untyped =
+    # For CPU:
+    var rCPU: EC_ShortW_Jac[field, G1]
+    rCPU = a + b
+
+    # For GPU:
+    var rGPU: EC_ShortW_Jac[field, G1]
+    kernel.execCuda(res = rGPU, inputs = (a, b))
+
+    # Verify CPU and GPU agree
+    doAssert bool(rCPU == rGPU)
+
+    # return point
+    rGPU
+  var res = a
+  for i in 0 ..< 1000: # `res = res + b`, starting with `a + b`
+    res = checkSum(res, b)
+
+
 let x = "0x2ef34a5db00ff691849861d49415d8081d9d0e10cba33b57b2dd1f37f13eeee0"
 let y = "0x2beb0d0d6115007676f30bcc462fe814bf81198848f139621a3e9fa454fe8e6a"
 let pt = EC_ShortW_Jac[Fp[BN254_Snarks], G1].fromHex(x, y)
@@ -52,4 +78,9 @@ let x2 = "0x226c85cf65f4596a77da7d247310a81ac9aa9220e819e3ef23b6cbe0218ce272"
 let y2 = "0xf53265870f65aa18bded3ccb9c62a4d8b060a32a05a75d455710bce95a991df"
 let pt2 = EC_ShortW_Jac[Fp[BN254_Snarks], G1].fromHex(x2, y2)
 
-test(Fp[BN254_Snarks], 64, pt, pt2)
+testSum(Fp[BN254_Snarks], 64, pt, pt2)
+
+var pt2Aff: EC_ShortW_Aff[Fp[BN254_Snarks], G1]
+pt2Aff.affine(pt2)
+
+testMixedSum(Fp[BN254_Snarks], 64, pt, pt2Aff)
