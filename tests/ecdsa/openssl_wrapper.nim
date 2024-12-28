5229551d69afb50632cd6a4031b1c850f489fae6
Vindaar <basti90@gmail.com>
2024-12-28 12:22:02 +0100

    [tests] extend OpenSSL wrapper for required functionality

--- tests/ecdsa/openssl_wrapper.nim
+++ tests/ecdsa/openssl_wrapper.nim
@@ -2,9 +2,9 @@
 # --------------------------------------------------------------------
 when defined(windows):
   when sizeof(int) == 8:
-    const DLLSSLName* = "(libssl-1_1-x64|ssleay64|libssl64).dll"
+    const DLL_SSL_Name* = "(libssl-1_1-x64|ssleay64|libssl64).dll"
   else:
-    const DLLSSLName* = "(libssl-1_1|ssleay32|libssl32).dll"
+    const DLL_SSL_Name* = "(libssl-1_1|ssleay32|libssl32).dll"
 else:
   when defined(macosx) or defined(macos) or defined(ios):
     const versions = "(.1.1|.38|.39|.41|.43|.44|.45|.46|.47|.48|.10|.1.0.2|.1.0.1|.1.0.0|.0.9.9|.0.9.8|)"
@@ -12,11 +12,11 @@ else:
     const versions = "(.1.1|.1.0.2|.1.0.1|.1.0.0|.0.9.9|.0.9.8|.48|.47|.46|.45|.44|.43|.41|.39|.38|.10|)"
 
   when defined(macosx) or defined(macos) or defined(ios):
-    const DLLSSLName* = "libssl" & versions & ".dylib"
+    const DLL_SSL_Name* = "libssl" & versions & ".dylib"
   elif defined(genode):
-    const DLLSSLName* = "libssl.lib.so"
+    const DLL_SSL_Name* = "libssl.lib.so"
   else:
-    const DLLSSLName* = "libssl.so" & versions
+    const DLL_SSL_Name* = "libssl.so" & versions
 
 # OpenSSL wrapper
 # --------------------------------------------------------------------
@@ -30,7 +30,7 @@ when not defined(windows):
   proc SHA256[T: byte|char](
         msg: openarray[T],
         digest: ptr array[32, byte] = nil
-      ): ptr array[32, byte] {.noconv, dynlib: DLLSSLName, importc.}
+      ): ptr array[32, byte] {.noconv, dynlib: DLL_SSL_Name, importc.}
 
   # proc EVP_Q_digest[T: byte|char](
   #                 ossl_libctx: pointer,
@@ -38,9 +38,9 @@ when not defined(windows):
   #                 propq: cstring,
   #                 data: openArray[T],
   #                 digest: var array[32, byte],
-  #                 size: ptr uint): int32 {.noconv, dynlib: DLLSSLName, importc.}
+  #                 size: ptr uint): int32 {.noconv, dynlib: DLL_SSL_Name, importc.}
 
-  proc SHA256_OpenSSL[T: byte|char](
+  proc SHA256_OpenSSL*[T: byte|char](
         digest: var array[32, byte],
         s: openArray[T]) =
     discard SHA256(s, digest.addr)
@@ -59,13 +59,28 @@ type
   EVP_MD_CTX* = ptr EVP_MD_CTX_Obj
   EVP_PKEY_CTX* = ptr EVP_PKEY_CTX_Obj
 
-type
   BIGNUM* = ptr BIGNUM_Obj
   EC_KEY* = ptr EC_KEY_Obj
   EC_GROUP* = ptr EC_GROUP_Obj
 
+  BIO_Obj* = object
+  BIO* = ptr BIO_Obj
+
+  OSSL_ENCODER_CTX_Obj*  = object
+  OSSL_ENCODER_CTX* = ptr OSSL_ENCODER_CTX_Obj
+
+  OSSL_LIB_CTX_Obj* = object
+  OSSL_PROVIDER_Obj* = object
+  OSSL_LIB_CTX* = ptr OSSL_LIB_CTX_Obj
+  OSSL_PROVIDER* = ptr OSSL_PROVIDER_Obj
+  OSSL_PARAM_Obj* = object
+  OSSL_PARAM* = ptr OSSL_PARAM_Obj
+
+  OSSL_PARAM_BLD_Obj* = object
+  OSSL_PARAM_BLD* = ptr OSSL_PARAM_BLD_Obj
+
 ## Push the pragmas to clean up the code a bit
-{.push noconv, importc, dynlib: DLLSSLName.}
+{.push noconv, importc, dynlib: DLL_SSL_Name.}
 proc EVP_MD_CTX_new*(): EVP_MD_CTX
 proc EVP_MD_CTX_free*(ctx: EVP_MD_CTX)
 
@@ -99,6 +114,9 @@ proc EVP_PKEY_new*(): EVP_PKEY
 ## in theory:
 ## https://docs.openssl.org/master/man3/EVP_PKEY_fromdata/
 proc EVP_PKEY_set1_EC_KEY*(pkey: EVP_PKEY, key: EC_KEY): cint
+
+proc BIO_new_file*(filename: cstring, mode: cstring): BIO
+proc BIO_free*(bio: BIO): cint
 {.pop.}
 
 proc initPrivateKeyOpenSSL*(pkey: var EVP_PKEY, rawKey: openArray[byte]) =
