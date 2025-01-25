564cc427651533fbaed250c7a25e4cb8161ce848
Vindaar <basti90@gmail.com>
2025-01-25 13:43:27 +0100

    clean up FFT LUT calculation code

--- constantine/math/polynomials/fft_lut.nim
+++ constantine/math/polynomials/fft_lut.nim
@@ -62,7 +62,6 @@ proc decomposeFieldOrder(F: type Fr): tuple[s: F, e: uint64] =
   # `s = p - 1`
   var s {.noInit.}: BigInt[F.bits()]
   s = F.getModulus()
-  debugecho "Exp: ", s.tohex()
   s -= One
 
   # `e = 0`
@@ -70,7 +69,6 @@ proc decomposeFieldOrder(F: type Fr): tuple[s: F, e: uint64] =
 
   while s.isEven().bool:
     s.shiftRight(1)
-    #e += F.fromUint(1'u64)
     inc e
   result = (s: F.fromBig(s), e: e)
 
@@ -90,47 +88,20 @@ proc findAnyQnr(F: type Fr): F =
 func buildRootLUT(F: type Fr, primitive_root: uint64): array[32, F] =
   ## [pow(PRIMITIVE_ROOT, (MODULUS - 1) // (2**i), MODULUS) for i in range(32)]
 
-  ##
-  ## XXX: For some reason this does not work for BN254_Snarks (haven't tried with BLS12-381)
-
   let (s, e) = decomposeFieldOrder(F)
   let qnr = findAnyQnr(F)
 
-  #var exponent {.noInit.}: BigInt[F.bits()]
-  #exponent = F.getModulus()
-  #debugecho "Exp: ", exponent.tohex()
-  #exponent -= One
-  #
-  ## Start by the end
-  #var i = result.len - 1
-  #exponent.shiftRight(i)
-  #debugecho "last Exponent : ", exponent.toHex()
-  #result[i].fromUint(primitive_root)
-  #result[i].pow_vartime(exponent)
-
-
-  var i = e.int #e.toDecimal() # largest possible power of 2 for this field
+  var i = e.int
   var rootUnity = qnr
   rootUnity.pow_vartime(s)
   result[i] = rootUnity
 
-
   while i > 0:
     result[i-1].square(result[i])
-    #debugecho "At ", i, ": ", result[i-1].toHex()
-
     dec i
-
-  # debugEcho "Fr[BLS12_81] - Roots of Unity:"
-  # for i in 0 ..< result.len:
-  #   debugEcho "    ", i, ": ", result[i].toHex()
-  # debugEcho "Fr[BLS12_81] - Roots of Unity -- FIN\n"
 
 #let BLS12_381_Fr_ScaleToRootOfUnity* = buildRootLUT(Fr[BLS12_381], BLS12_381_Fr_primitive_root)
 let BN254_Snarks_Fr_ScaleToRootOfUnity* = buildRootLUT(Fr[BN254_Snarks], BN254_Snarks_Fr_primitive_root)
-let g2 = BN254_Snarks_Fr_ScaleToRootOfUnity # [28 - order]
-for i, el in g2:
-  debugecho "ω^{2^", i, "} = ", el.toHex()
 
 {.experimental: "dynamicBindSym".}
 macro scaleToRootOfUnity*(Name: static Algebra): untyped =
