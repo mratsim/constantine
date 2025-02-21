3e6a8775e5f450270390fd5e349e0c9f2c844aa7
Vindaar <basti90@gmail.com>
2025-02-21 19:48:30 +0100

    [tests] add test to pass by pointer and `var`

---
+++ tests/gpu/t_nvrtc_pass_var.nim
@@ -0,0 +1,57 @@
+import std / strformat
+
+import
+  # Internal
+  constantine/named/algebras,
+  constantine/math/io/[io_bigints, io_fields],
+  constantine/math/arithmetic,
+  constantine/platforms/abstractions {.all.},
+  constantine/platforms/abis/nvidia_abi,
+  constantine/math_compiler/experimental/runtime_compile,
+  constantine/serialization/io_limbs
+
+import constantine/math_compiler/experimental/nvrtc_field_ops
+
+const N = 8
+type T = Fp[BN254_Snarks]
+const WordSize = 32
+const BigIntExample* = cuda:
+
+  defBigInt(N)
+  defPtxHelpers()
+  defCoreFieldOps()
+
+  proc testPointer(x: var BigInt) {.device.} =
+    ## just write data to the bigint
+    for i in 0 ..< 8:
+      x[i] = i.uint32
+
+  proc test(output: ptr UncheckedArray[uint32]) {.global.} =
+    # Call `modadd` and assign to result
+
+    var t = BigInt()
+
+    testPointer(t)
+
+    for i in 0 ..< 8:
+      output[i] = t[i]
+
+proc main =
+  var nvrtc = initNvrtc(BigIntExample)
+  # echo the generated CUDA code
+  echo BigIntExample
+
+  nvrtc.compile()
+  nvrtc.getPtx()
+
+  var hOut: array[8, uint32] # storage for the output limbs (could also be a `Fp[BN254_Snarks]` instead)
+  nvrtc.numBlocks = 1
+  nvrtc.threadsPerBlock = 1
+
+  nvrtc.execute("test", (hOut), ())
+  for i in 0 ..< 8:
+    echo hOut[i]
+
+
+when isMainModule:
+  main()
